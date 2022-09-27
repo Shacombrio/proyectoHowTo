39f437f4b333c748fe9415cc922d76db266c2bcf
Shacombrio <102573101+Shacombrio@users.noreply.github.com>
2022-09-26 21:40:24 -0600

    equisde

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:LoginComponent}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
