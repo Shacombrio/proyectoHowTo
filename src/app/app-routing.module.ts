14274e2ef9a311ae3d617f99dcf3c3e15b00c495
axelito-sl <sanchez.axel3amp@gmail.com>
2022-09-30 23:44:09 -0600

    1/10/22
    meti el routing ese que les enseñé nomás ajajajks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarperfilComponent } from './editarperfil/editarperfil.component';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
-  {path:"home",component:LoginComponent}
+  {path:"home",component:RegistroComponent},
+  {path:"editarperfil",component:EditarperfilComponent}
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(routes);
