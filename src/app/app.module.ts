a8bc2eaa949913d618f1db70ae741353d3808380
dimasmnat <114051480+dimasmnat@users.noreply.github.com>
2022-09-30 23:19:45 -0600

    30/09/22-11:18PM
    Creé  los componentes: login, registro y editar perfil.  Creé el front en html y css, para cada componente.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {RegistroComponent} from './registro/registro.component';
+import { EditarperfilComponent } from './editarperfil/editarperfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegistroComponent,
+    EditarperfilComponent
   ],
   imports: [
     BrowserModule,
